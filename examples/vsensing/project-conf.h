9c3fb7c9131e18a8dcf894a768c006af6def5a70
Fernando Iglesias <iglesiasfnj@gmail.com>
2020-08-14 01:03:48 -0300

    Add fixes.

---
+++ examples/vsensing/project-conf.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+// For 2.4 GHz (IEEE) Mode
+//#define RF_CONF_MODE    RF_MODE_2_4_GHZ
+// For Sub-1 GHz (Prop) Mode
+#define RF_CONF_MODE    RF_MODE_SUB_1_GHZ
+
+#endif /* PROJECT_CONF_H_ */
